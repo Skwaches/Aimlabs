8987bbc086857d19ee43d823e1472620a5e65b02
skwaches <jaduarokoth@gmail.com>
2025-08-26 21:18:27 +0300

    some how messed up previous commit bruh

--- highscoreSDK.py
+++ highscoreSDK.py
@@ -4,7 +4,7 @@ def cursor():
     return conn.cursor()
     
 c = cursor()
-c.execute('CREATE TABLE IF NOT EXISTS highscores (username TEXT, score INTEGER)')
+c.execute('CREATE TABLE IF NOT EXISTS highscores (username TEXT, score FlOAT)')
 
 def user_indb(user:str):
     c = cursor()
@@ -14,7 +14,7 @@ def user_indb(user:str):
     c.close()
     return prescence
 
-def score_indb(score:int):
+def score_indb(score:float):
     c = cursor()
     with c.connection:
         c.execute('SELECT 1 FROM highscores WHERE score = ?',(score,))
@@ -25,26 +25,41 @@ def score_indb(score:int):
 def all_users():
     c = cursor()
     all_users = []
-    seen = set[int]()
+    seen = set[float]()
     with c.connection:
         c.execute('SELECT username FROM highscores')
         all_users = [user[0] for user in c.fetchall() if user not in seen and not seen.add(user)]
     c.close()
     return all_users
+def all_scores():
+    c = cursor()
+    all_scores = []
+    with c.connection:
+        c.execute('SELECT score FROM highscores')
+        all_scores = c.fetchall()
+    all_scores = list(set(all_scores))
+    all_scores = [all_score[0] for all_score in all_scores]
+    all_scores.sort(reverse=True)
+    c.close
+    
+    return all_scores
 
-def add_scores(user:str,score:int):
+def add_scores(user:str,score:float):
+    
     try:
         c = cursor()
         with c.connection:
             c.execute('INSERT INTO highscores VALUES (?,?)',(user,score))
         c.close()
+        print("Score added!")
     except Exception as e:
         print(e)
 
+
 def get_by_user(users:list[str]):
     c = cursor()
-    all_relevant_values = list[tuple[str,int]]()
-    user_dict = dict[str,list[int]]()
+    all_relevant_values = list[tuple[str,float]]()
+    user_dict = dict[str,list[float]]()
     for user in users:
         user_dict[user] = []
         with c.connection:
@@ -57,9 +72,9 @@ def get_by_user(users:list[str]):
 
     return user_dict
 
-def get_by_score(scores:list[int]):
+def get_by_score(scores:list[float]):
     c = cursor()
-    legends = dict[int,list[str]]()
+    legends = dict[float,list[str]]()
     for score in scores:
         
         with c.connection:
@@ -72,7 +87,7 @@ def get_by_score(scores:list[int]):
 
 def get_all():
     c = cursor()
-    all_dict = dict[str,list[int]]()
+    all_dict = dict[str,list[float]]()
     with c.connection:
         c.execute('SELECT * FROM highscores')
         data = c.fetchall()
@@ -87,7 +102,7 @@ def get_all():
 
     return all_dict
 
-def del_user_score(user:int):
+def del_user_score(user:float):
     c =cursor()
     with c.connection:
         c.execute('DELETE FROM highscores WHERE username = ?',(user,))
@@ -98,6 +113,7 @@ def del_all():
     with c.connection:
         c.execute('DELETE FROM highscores')
     c.close()
+    
 
 def my_searcher(text:str):
     users = all_users()
@@ -110,18 +126,22 @@ def my_searcher(text:str):
     return suggestions
 
 def top(x:int):
-    score_board = get_all()
-    top_scores  = list[int]()
-    for user in score_board:
-        temp_score = max(score_board[user])
-        if len(top_scores)<x:
-            top_scores.append(temp_score)
-        elif temp_score >= min(top_scores):
-            top_scores.append(temp_score)
-            top_scores.remove(min(top_scores))
-    top_scores.sort(reverse=True)
-    return get_by_score(top_scores)
-            
+    all_scores_list  = all_scores()[:x] if len(all_scores())>= x else all_scores()
+    score_board = get_by_score(all_scores_list)
+    min_score = int()
+    legends = []
+    for score in score_board:
+        if len(legends)< x: 
+            legends += score_board[score]
+            min_score = score
+        else:
+            break
+    to_del = [score for score in score_board if score<min_score]
+    for score in to_del:
+        if score in score_board:
+            del score_board[score]
+
+    return score_board
 
 if __name__ == "__main__":
-    pass
+    print(top(10))
